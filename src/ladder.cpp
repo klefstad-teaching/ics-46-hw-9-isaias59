4ec85556f5a47c7ec3c46f215a448f07bbe41cef
isaias59 <127485411+isaias59@users.noreply.github.com>
2025-03-08 19:54:24 -0800

    Update ladder.cpp

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -90,9 +90,9 @@ void print_word_ladder(const vector<string>& ladder)
         return;
     }
 
+    cout << "Word ladder found: ";
     for (size_t i = 0; i < ladder.size(); ++i) {
-        cout << ladder[i];
-        if (i < ladder.size() - 1) cout << " -> ";
+        cout << ladder[i] << " ";
     }
     cout << '\n';
 }
